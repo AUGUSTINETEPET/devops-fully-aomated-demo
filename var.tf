c2eceeb94cb42ad7783678a9319e6a4b0b05e2c6
AUGUSTINETEPET <azaah1975@gmail.com>
2022-12-10 16:16:09 -0500

    demo

--- var.tf
+++ var.tf
@@ -1,7 +1,7 @@
 variable "key_pair_name" {
     type = string
-    description = "jenkinsfullydemokeppair"
-    default = "AUGUSTINE-TEPET"
+    description = "dev test"
+    default = "jenkinsfullydemokeppairT"
 }
 
 # variable "vpc_id" {
